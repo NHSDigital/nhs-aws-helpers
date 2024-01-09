13986ede2227f223283ea33a4cc1187e72eba649
susana-zhou1 <susana.zhou1@nhs.net>
2024-01-09 10:47:14 +0000

    SPINECORE-3459: input whole url rather than just vpc_endpoint

--- nhs_aws_helpers/__init__.py
+++ nhs_aws_helpers/__init__.py
@@ -1045,7 +1045,7 @@ def assumed_credentials(
     role: str,
     role_session_name: Optional[str] = None,
     duration_seconds: int = 1200,
-    vpc_endpoint: Optional[str] = None,
+    sts_endpoint_url: Optional[str] = None,
 ):
     """
     Refreshes the IAM credentials provided to us by STS for the duration of our session. This
@@ -1057,10 +1057,7 @@ def assumed_credentials(
          expiration timestamp for the session.
     """
     region = os.environ.get("AWS_REGION", "eu-west-2")
-    if vpc_endpoint:
-        endpoint_url = f"https://vpce.{vpc_endpoint}.sts.{region}.amazonaws.com"
-    else:
-        endpoint_url = f"https://sts.{region}.amazonaws.com"
+    endpoint_url = sts_endpoint_url or f"https://sts.{region}.amazonaws.com"
 
     role_session_name = role_session_name or f"assumed-{uuid4().hex}"
 
@@ -1089,7 +1086,7 @@ def assumed_role_session(
     role_session_name: Optional[str] = None,
     duration_seconds: int = 1200,
     refreshable: bool = False,
-    vpc_endpoint: Optional[str] = None,
+    sts_endpoint_url: Optional[str] = None,
 ) -> Session:
     """
     gets assumed role session for given account id and role name
@@ -1102,7 +1099,7 @@ def assumed_role_session(
         role,
         role_session_name=role_session_name,
         duration_seconds=duration_seconds,
-        vpc_endpoint=vpc_endpoint,
+        sts_endpoint_url=sts_endpoint_url,
     )
 
     credentials = get_credentials()
